9de9881afe1dc55d0c5ba2e7a62e967581c97d24
FrancooRoldan <fr.roldan@alumno.um.edu.ar>
2024-04-17 17:47:40 -0300

    Inicialización del proyecto

---
+++ src/test/java/Group/ClientManager/ClientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.ClientManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
